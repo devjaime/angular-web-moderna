21866bf429ba5e373ce99ed278b28e3b9cd665b8
Jaime Hernandez <hernandez.hs@gmail.com>
2019-10-29 01:15:47 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  appID: '6afa38f1264dd4178932af31628e0bbb',
+  gKey: '6afa38f1264dd4178932af31628e0bbb',
+  config: {
+    apiKey: 'AIzaSyAa98TaJff4H_q_8Ao2Fk4Ci1g6jdvYRY0',
+    authDomain: 'demoangular-67df5.firebaseapp.com',
+    databaseURL: 'https://demoangular-67df5.firebaseio.com',
+    projectId: 'demoangular-67df5',
+    storageBucket: 'demoangular-67df5.appspot.com',
+    messagingSenderId: '"G-02FLSG7W22'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,26 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appID: '6afa38f1264dd4178932af31628e0bbb',
+  gKey: '6afa38f1264dd4178932af31628e0bbb',
+  config: {
+    apiKey: 'AIzaSyAa98TaJff4H_q_8Ao2Fk4Ci1g6jdvYRY0',
+    authDomain: 'demoangular-67df5.firebaseapp.com',
+    databaseURL: 'https://demoangular-67df5.firebaseio.com',
+    projectId: 'demoangular-67df5',
+    storageBucket: 'demoangular-67df5.appspot.com',
+    messagingSenderId: '"G-02FLSG7W22'
+  }
+
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
